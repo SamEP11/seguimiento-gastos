6020748e67839871a3d22a96a5608947fe87a9ac
Samuel Echeverry Pineda <samuelecheverrypineda@gmail.com>
2025-07-08 14:52:17 -0500

    fix: Corregir rutas en el script de construcción

--- build.sh
+++ build.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# 1. Instala todas las dependencias listadas en requirements.txt
 pip install -r requirements.txt
-
-# 2. Recolecta todos los archivos estáticos (CSS, JS, etc.) en una sola carpeta
-python config/manage.py collectstatic --noinput
-
-# 3. Aplica las migraciones de la base de datos
-python config/manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py migrate
